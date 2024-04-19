e5bb844d85522db46364405f836adf3f36768b96
DiogoKaster <diogokaster@gmail.com>
2024-04-19 09:50:55 -0300

    feat: Upload photo functionality added

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,32 +1,86 @@
-import { TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { Alert, TouchableOpacity } from "react-native";
 
 import {
   Center,
   Heading,
   ScrollView,
   Text,
+  Toast,
+  ToastDescription,
+  ToastTitle,
   VStack,
   View,
+  useToast,
 } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/DiogoKaster.png"
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        if (Number(photoSelected.assets[0].fileSize) / 1024 / 1024 > 5) {
+          toast.show({
+            placement: "top",
+            render: ({ id }) => {
+              const toastId = "toast-" + id;
+              return (
+                <Toast
+                  bg="$error500"
+                  nativeID={toastId}
+                  action="attention"
+                  variant="solid"
+                >
+                  <VStack space="xs">
+                    <ToastTitle>Erro</ToastTitle>
+                    <ToastDescription>
+                      Imagem não pode ter mais que 5MB.
+                    </ToastDescription>
+                  </VStack>
+                </Toast>
+              );
+            },
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
       <ScrollView>
         <Center mt="$6" px="$10">
-          <UserPhoto
-            source={{ uri: "https://github.com/DiogoKaster.png" }}
-            alt="user-photo"
-            size={135}
-          />
+          <UserPhoto source={{ uri: userPhoto }} alt="user-photo" size={135} />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontWeight="$bold"
